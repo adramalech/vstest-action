8b4d19bfa6199e1251b58b2a91034402e2d974c9
Jonathan Throne <jthrone@ultrasignup.com>
2024-09-06 14:40:26 -0700

    Fix for bug.

--- src/index.ts
+++ src/index.ts
@@ -21,14 +21,17 @@ export async function run() {
     const vsTestPath = getVsTestPath();
     core.debug(`VsTestPath: ${vsTestPath}`);
 
-    const output = await exec.getExecOutput(`powershell Test-Path -Path ${vsTestPath}`);
+    let toolAlreadyUnarchived: boolean;
 
-    const toolAlreadyUnarchived = (output.stdout ?? '').toUpperCase().trim() === 'TRUE';
+    try {
+      const output = await exec.getExecOutput(`powershell Test-Path -Path ${vsTestPath}`);
+      toolAlreadyUnarchived = (output.stdout ?? '').toUpperCase().trim() === 'TRUE';
+    } catch {
+      toolAlreadyUnarchived = false;
+    }
 
     // if the test tools already exist in the target folder do not try to overwrite them.
-    if (toolAlreadyUnarchived) {
-      core.info(`Test tool exists already skipping unarchiving it...`);
-    } else {
+    if (!toolAlreadyUnarchived) {
       core.info(`Setting test tools...`);
       const workerZipPath = path.join(__dirname, 'win-x64.zip');
 
